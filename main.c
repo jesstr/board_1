97b9be833928d2e90135b77ca2aab6f15ef6f6f0
Pavel Cherstvov <jesstr@mail.ru>
2014-08-12 23:47:45 +0400

    test for shift registers

--- main.c
+++ main.c
@@ -5,11 +5,110 @@
  *      Author: Pavel Cherstvov
  */
 
-
 #include <avr/io.h>
 #include <stdlib.h>
+#include <util/delay.h>
+
+#define PR_CLK_PIN	PG0
+#define PR_CLK_PORT	PORTG
+#define PR_CLK_DDR	DDRG
+
+#define PR_RES_PIN	PG1
+#define PR_RES_PORT	PORTG
+#define PR_RES_DDR	DDRG
+
+#define AD1_PIN		PC0
+#define AD1_PORT	PORTC
+#define AD1_DDR		DDRC
+
+#define AD2_PIN		PC1
+#define AD2_PORT	PORTC
+#define AD2_DDR		DDRC
+
+#define AD3_PIN		PC2
+#define AD3_PORT	PORTC
+#define AD3_DDR		DDRC
+
+#define AD4_PIN		PC3
+#define AD4_PORT	PORTC
+#define AD4_DDR		DDRC
+
+#define CLOCK_OUT_DATA 	do { 								\
+						PR_CLK_PORT |= (1 << PR_CLK_PIN); 	\
+						_delay_us(1);						\
+						PR_CLK_PORT &= ~(1 << PR_CLK_PIN); 	\
+						_delay_us(1);						\
+						} while (0)
+
+/* Data array for shift registers */
+unsigned char LedDataReg[4];
+
+
+inline void IO_Init(void) {
+	PR_CLK_DDR |= (1 << PR_CLK_PIN);
+	PR_RES_DDR |= (1 << PR_RES_PIN);
+	PR_RES_PORT |= (1 << PR_RES_PIN);
+	AD1_DDR |= (1 << AD1_PIN);
+	AD2_DDR |= (1 << AD2_PIN);
+	AD3_DDR |= (1 << AD3_PIN);
+	AD4_DDR |= (1 << AD4_PIN);
+}
+
+
+void UpdateShiftRegister(void) {
+	unsigned char i, out_data;
+
+	for (i = 0; i < 8; i++) {
+
+		out_data = (LedDataReg[0] >> i) & 0x01;
+		out_data |= ((LedDataReg[1] >> i) & 0x01) << 1;
+		out_data |= ((LedDataReg[2] >> i) & 0x01) << 2;
+		out_data |= ((LedDataReg[3] >> i) & 0x01) << 3;
+
+		PORTC = out_data;
+
+		CLOCK_OUT_DATA;
+
+		/*
+		AD1_PORT &= ~(1 << AD1_PIN);
+		AD1_PORT = (1 << AD1_PIN) &             (1 << i) & LedDataReg[0]                (LedDataReg[1] >> i) & 0x01;
+	 	*/
+	}
+}
 
 
 int main(void) {
+	IO_Init();
+/*
+	LedDataReg[0] = 0x01;
+	LedDataReg[1] = 0x03;
+	LedDataReg[2] = 0x07;
+	LedDataReg[3] = 0x0F;
+*/
+	LedDataReg[0] = 0x00;
+	LedDataReg[1] = 0x00;
+	LedDataReg[2] = 0x00;
+	LedDataReg[3] = 0x00;
 
+	unsigned char i,j;
+
+	while (1) {
+		for (j = 1; j < 5; j++) {
+			for (i = 1; i < 9; i++) {
+				//LedDataReg[j - 1] = i * 2 * j - 1;
+				LedDataReg[j - 1] = (2 ^ i) - 1;
+				UpdateShiftRegister();
+				_delay_ms(1000);
+			}
+		}
+
+/*
+		LedDataReg[0] = 0xFF;
+		LedDataReg[1] = 0xFF;
+		LedDataReg[2] = 0xFF;
+		LedDataReg[3] = 0xFF;
+		UpdateShiftRegister();
+		_delay_ms(1000);
+*/
+	}
 }
